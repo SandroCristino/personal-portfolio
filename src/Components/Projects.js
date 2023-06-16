82054ab8210f887da846d1edc6de554fffa0110d
SandyWezzy <72725989+SandyWezzy@users.noreply.github.com>
2023-06-16 23:22:10 +0800

    add more projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import redditVideo from '../Assets/redditPresentation.mov'
+import todoVideo from '../Assets/todoPresentation.mov'
+import memoryVideo from '../Assets/memorycardPresentation.mov'
 import reactIcon from '../Assets/Icons/react.png'
+import javascriptIcon from '../Assets/Icons/javascript.png'
 import bootstrapIcon from '../Assets/Icons/bootstrap.png'
 import '../Styles/Projects.css'
 
@@ -26,6 +29,108 @@ export default function Projects() {
         <div className='projects-outer-container' id='projects'>
             <h2 className={`projects-headline ${isScrolled ? 'animate-scroll' : ''}`}>Projects</h2>
             <hr />
+
+            {/* Project one - reddit */}
+            <div className="project">
+                <h3 className='project-headline'>Reddit</h3>
+                <h4 className='project-subline'>Social Media Clone</h4>
+                <div className='project-main'>
+                    <video className='project-video' autoPlay loop muted src={redditVideo}></video>
+                    <div className='project-description'>
+                        <div>
+                            <h5 className='project-description-headline'>Summary</h5>
+                            <p className='project-description-info'>A Full Stack clone of Reddit's features and a complete redesign of the UI. Originally built with firebase, it was rebuilt using React and Bootstrap for the frontend</p>
+                        </div>
+                        <div>
+                            <h6 className='project-description-technologie'>Technologies</h6>
+                            <div className='project-description-frameworks'>
+                                <img className='project-description-img' src={reactIcon} alt="React" />
+                                <img className='project-description-img' src={bootstrapIcon} alt="Bootstrap" />
+                                <img className='project-description-img' src={javascriptIcon} alt="Javascript" />
+                            </div>
+                        </div>
+                        <div className='project-buttons'>
+                            <div className='project-button-group' onClick={() => window.location.href = 'https://sandrocristino.github.io/reddit_clone/'}>
+                                <button className='project-button'>LIVE PREVIEW</button>
+                                <span class="material-symbols-outlined">arrow_circle_right</span>
+                            </div>
+                            <div className='project-button-group' onClick={() => window.location.href = 'https://github.com/SandroCristino/reddit_clone'}>
+                                <button className='project-button'>VIEW CODE</button>
+                                <span class="material-symbols-outlined">arrow_circle_right</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+
+            {/* Project two - todo list */}
+            <div className="project">
+                <h3 className='project-headline'>ToDo List</h3>
+                <h4 className='project-subline'>Planner</h4>
+                <div className='project-main'>
+                    <video className='project-video' autoPlay loop muted src={todoVideo}></video>
+                    <div className='project-description'>
+                        <div>
+                            <h5 className='project-description-headline'>Summary</h5>
+                            <p className='project-description-info'>A Full Stack ToDo List application that allows users to create tasks, assign dates to them, sort the tasks based on various criteria, and remove tasks when necessary. Develop the application using pure JavaScript without any external frameworks or libraries.</p>
+                        </div>
+                        <div>
+                            <h6 className='project-description-technologie'>Technologies</h6>
+                            <div className='project-description-frameworks'>
+                                <img className='project-description-img' src={javascriptIcon} alt="Javascript" />
+                            </div>
+                        </div>
+                        <div className='project-buttons'>
+                            <div className='project-button-group' onClick={() => window.location.href = 'https://sandrocristino.github.io/toDo_list/'}>
+                                <button className='project-button'>LIVE PREVIEW</button>
+                                <span class="material-symbols-outlined">arrow_circle_right</span>
+                            </div>
+                            <div className='project-button-group' onClick={() => window.location.href = 'https://github.com/SandroCristino/toDo_list'}>
+                                <button className='project-button'>VIEW CODE</button>
+                                <span class="material-symbols-outlined">arrow_circle_right</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+
+            {/* Project three - memory_card */}
+            <div className="project">
+                <h3 className='project-headline'>Memory Card</h3>
+                <h4 className='project-subline'>Test Your Memory</h4>
+                <div className='project-main'>
+                    <video className='project-video' autoPlay loop muted src={memoryVideo}></video>
+                    <div className='project-description'>
+                        <div>
+                            <h5 className='project-description-headline'>Summary</h5>
+                            <p className='project-description-info'>A Full Stack memory game with singleplayer functionality: Test your ability to remember as many cards as possible. However, be cautious, as the alignment of the cards changes after every click. Avoid clicking on the same card twice. The game is built using React and Bootstrap.</p>
+                        </div>
+                        <div>
+                            <h6 className='project-description-technologie'>Technologies</h6>
+                            <div className='project-description-frameworks'>
+                                <img className='project-description-img' src={reactIcon} alt="React" />
+                                <img className='project-description-img' src={bootstrapIcon} alt="Bootstrap" />
+                                <img className='project-description-img' src={javascriptIcon} alt="Javascript" />
+                            </div>
+                        </div>
+                        <div className='project-buttons'>
+                            <div className='project-button-group' onClick={() => window.location.href = 'https://sandrocristino.github.io/memory_card/'}>
+                                <button className='project-button'>LIVE PREVIEW</button>
+                                <span class="material-symbols-outlined">arrow_circle_right</span>
+                            </div>
+                            <div className='project-button-group' onClick={() => window.location.href = 'https://github.com/SandroCristino/memory_card'}>
+                                <button className='project-button'>VIEW CODE</button>
+                                <span class="material-symbols-outlined">arrow_circle_right</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+
+
             <div className="project">
                 <h3 className='project-headline'>Reddit</h3>
                 <h4 className='project-subline'>Social Media Clone</h4>
@@ -44,11 +149,11 @@ export default function Projects() {
                             </div>
                         </div>
                         <div className='project-buttons'>
-                            <div className='project-button-group'>
+                            <div className='project-button-group' onClick={() => window.location.href = 'https://sandrocristino.github.io/reddit_clone/'}>
                                 <button className='project-button'>LIVE PREVIEW</button>
                                 <span class="material-symbols-outlined">arrow_circle_right</span>
                             </div>
-                            <div className='project-button-group'>
+                            <div className='project-button-group' onClick={() => window.location.href = 'https://github.com/SandroCristino/reddit_clone'}>
                                 <button className='project-button'>VIEW CODE</button>
                                 <span class="material-symbols-outlined">arrow_circle_right</span>
                             </div>
@@ -56,7 +161,6 @@ export default function Projects() {
                     </div>
                 </div>
             </div>
-            <hr />
         </div>
     )
 }
