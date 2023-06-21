a961f045b523b07abd400d8217c2dd7f015132d3
SandyWezzy <72725989+SandyWezzy@users.noreply.github.com>
2023-06-21 15:54:45 +0800

    add responsive about.js

--- src/Components/FrameworkSlider.js
+++ src/Components/FrameworkSlider.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Carousel from 'react-slick'
+import '../Styles/FrameworkSlider.css'
 
 export default function FrameworkSlider() {
     const [images, setImages] = useState([])
@@ -19,21 +20,8 @@ export default function FrameworkSlider() {
         speed: 5000,
         autoplaySpeed: 2000,
         arrows: false,
-        pauseOnHover: true,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-            },
-          },
-        ],
+        pauseOnHover: true
+      
     };
 
  
